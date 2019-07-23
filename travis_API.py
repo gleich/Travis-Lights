c21eb58a2064d7160ffca456c08a759eba81bacf
Matthew Gleich <matthewgleich@gmail.com>
2019-07-23 18:55:35 -0400

    Create travis_API.py

---
+++ travis_API.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+import util
+
+
+# curl -L http://api.travis-ci.org/repos/goffstown-sports-app/Scrape-Calendar-Data
+
+def get_build_repo_status(repo_id):
+    """
+    Get the last build for the repo
+    :param repo_id: ID for the repo. Example: Matt-Gleich/Photo-Sort
+    :return: last_build_status
+    """
+    # Type Checking:
+    util.check_type(repo_id, "str")
+
+    # Querying information
+    url = "http://api.travis-ci.org/repos/" + repo_id
+    session = requests.Session()
+    req = requests.Request("GET", url)
+    prepped = session.prepare_request(req)
+    resp = session.send(prepped)
+    session.close()
+    resp_json = json.loads(resp.content.decode("utf-8"))
+    return resp_json["last_build_result"]
+
+# Testing:
+# print(get_build_repo_status("goffstown-sports-app/Scrape-Calendar-Data"))
